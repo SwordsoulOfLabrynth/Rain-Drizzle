f889135ea16ecf3475491cb4c40cb1d451176c11
Kyaw Zay Ya Hein <kyawzayyahein.mdy@gmail.com>
2025-01-09 23:31:48 +0630

    Fix [describe what you fixed]

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 const CACHE_NAME = "pwa-cache-v1";
-const urlsToCache = ["/", "/index.html", "/styles.css", "/app.js"];
+const urlsToCache = ["/", "/index.html"];
 
 // Install event
 self.addEventListener("install", (event) => {
